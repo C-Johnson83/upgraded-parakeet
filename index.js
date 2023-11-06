c943593da534e871e03c749dee007536375dc63e
chris <chrisj21283@gmail.com>
2023-11-06 12:18:15 -0500

    add comments and init function

--- index.js
+++ index.js
@@ -44,30 +44,34 @@ function doItToIt() {
 
 // function to write a file and save it
 function scribbleDown(data) {
-    let shapeString = '<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"><g>';
-    let end = '</svg>';
+    let shapeString = '<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg"><g>';// the g tag allows grouping of SVG elements. This is used to overlay the text on the shape
+    let end = '</svg>'; // to close the svg element
     let shapeChoice = `${data.shape}`
     let shapeOutput;
-    let userText = `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${data.textColor}">${data.text}</text></g>`;
+    let userText = `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${data.textColor}">${data.text}</text></g>`; // sets text location and size and color and closes the group tag
    
     
     if (shapeChoice === 'Triangle') {
         shapeOutput = new Triangle();
-        shapeString += `<polygon points="150, 18 244, 182 56, 182" fill="${data.shapeColor}"/>`;
+        shapeString += `<polygon points="150, 18 244, 182 56, 182" fill="${data.shapeColor}"/>`; // sets triangle size and color
     } else if (shapeChoice === 'Circle') {
         shapeOutput = new Circle();
-        shapeString += `<circle cx="150" cy="115" r="80" fill="${data.shapeColor}"/>`;
+        shapeString += `<circle cx="150" cy="115" r="80" fill="${data.shapeColor}"/>`; // sets circle size and color
     } else{
         shapeOutput = new Square();
-        shapeString += `<rect x="70" y="40" width="150" height="150" fill="${data.shapeColor}"/>`
+        shapeString += `<rect x="70" y="40" width="150" height="150" fill="${data.shapeColor}"/>` //sets square(rectangle) size and color
     }    
 
-
+    // combine all strings to make 1 SVG element
         svgString = shapeString+ userText + end
            // Create an svg file with the users shape, text, and color choices
     fs.writeFile("logo.svg", svgString, (err) => {
+        // if writeFile errors err is logged, otherwise Generated logo.SVG is logged
         err ? console.log(err) : console.log("Generated logo.svg");
     });
 }
+function init() {
     doItToIt();
-    
+}
+
+init()
