31f1a9292352e0c4dde50ad1f99e21ebfbac8636
chris <chrisj21283@gmail.com>
2023-11-06 09:32:55 -0500

    fixed typos in questions array

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inq = require('inquirer');
 const fs = require('fs');
 
+
 // Create question array for prompt
 const questions = [
     // Ask what text on the logo
@@ -13,20 +14,20 @@ const questions = [
     // Ask what color text on the logo
     {
         type: "input",
-        message: "Please choose a text color. You may enter a color keyword, or use a hexcode string",
+        message: "Please choose a text color. You may enter a color keyword, or use a hex code string",
         name: "textColor",
     },
     // Ask which shape to use for the logo
     {
         type: "list",
         message: "What shape would you like the logo to be?",
-        choices: ["Triangle", "Circle Square"],
+        choices: ["Triangle", "Circle, Square"],
         name: "shape",
     },
     // Ask what color the shape should be
     {
         type: "input",
-        message: "Please hoose a color for the shape. You may enter a color keyword, or use a hexcode string",
+        message: "Please choose a color for the shape. You may enter a color keyword, or use a hex code string",
         name: "shapeColor",
     },
 ];
@@ -38,6 +39,7 @@ function doItToIt() {
             console.log(data)
           
         })
-}
-
-doItToIt();
+    }
+    
+    doItToIt();
+    
